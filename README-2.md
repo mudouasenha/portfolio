35d6da14d7e66808f4e76b7abebcbb640f505c2b
mudouasenha <matheusgomesmtg1@gmail.com>
2025-01-11 15:39:49 -0300

    Project structure completed, missing text and assets

--- README-2.md
+++ README-2.md
@@ -1,5 +1,13 @@
+## Deploy:
+It was used Vercel to deploy this application.
+
+
 ## Useful Links and information:
+https://tailwindcss.com/ - tailwindcss for CSS styling simplified.
 https://fonts.google.com/ - Select the font, embed code and @import it into the index.css file.
 https://bg.ibelick.com/ - Background snippets, copy code and paste into the className attribute of the div
+https://motion.dev/ - Animations
+
 
 Assets: used for storing static files and images
+Components: the page block components used for page building
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
                 <Hero />
                 <About />
                 <Technologies />
+                <Experience />
+                <Projects />
+                <Contact />
             </div>
         </div>
     );
